56d879b4461e1b62ac1f65c0ed14851cbe2cc1d0
JeremyHTH <jeremy.hung56@gmail.com>
2023-09-17 00:58:49 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,150 @@
+const DisplayAllButton = document.getElementById("DisplayAll")
+const DisplayBreakfastButton = document.getElementById("DisplayBreakfast")
+const DisplayLunchButton = document.getElementById("DisplayLunch")
+const DisplayDinnerButton = document.getElementById("DisplayDinner")
+
+const AllList = document.querySelectorAll(".Item")
+const BreakfastList = document.querySelectorAll(".Item[Breakfast]")
+const LunchList = document.querySelectorAll(".Item[Lunch]")
+const DinnerList = document.querySelectorAll(".Item[Dinner]")
+
+const OrderList = document.getElementById("OrderList")
+const ConfirmButton = document.getElementById("ConfirmButton")
+const ClearButton = document.getElementById("ClearButton")
+
+console.log(AllList.length);
+console.log(BreakfastList.length);
+console.log(LunchList.length);
+console.log(DinnerList.length);
+
+AllList.forEach((Item, Index) =>{
+    Item.addEventListener('click', () =>
+    {
+        let Name = Item.querySelector(".ItemName").innerHTML;
+        console.log(Name);
+
+        const CurrOrder = document.createElement('div');
+		CurrOrder.classList.add('Order');
+
+        const OrderedItem = document.createElement('h1')
+        OrderedItem.classList.add("OrderName")
+        OrderedItem.innerHTML = Name
+        CurrOrder.appendChild(OrderedItem)
+
+        const CancelButton = document.createElement('button')
+        CancelButton.classList.add("CancelButton")
+        CancelButton.innerHTML = 'Cancel'
+        CurrOrder.appendChild(CancelButton)
+
+        OrderList.appendChild(CurrOrder)
+
+        CancelButton.addEventListener('click', () =>
+        {
+            OrderList.removeChild(CurrOrder)
+        })
+
+        OrderList.appendChild(CurrOrder)
+        
+        // let NewItem = `<div> <h1> ${Name} </h1> <button>Cancel</button> </div>`
+        // OrderList.innerHTML += NewItem
+        
+    })
+})
+
+DisplayAllButton.addEventListener('click',() =>
+{
+    SetBreakfastStatus(true)
+    SetLunchStatus(true)
+    SetDinnerStatus(true)
+})
+DisplayBreakfastButton.addEventListener('click',() =>
+{
+    SetBreakfastStatus(true)
+    SetLunchStatus(false)
+    SetDinnerStatus(false)
+})
+DisplayLunchButton.addEventListener('click',() =>
+{
+    SetBreakfastStatus(false)
+    SetLunchStatus(true)
+    SetDinnerStatus(false)
+})
+DisplayDinnerButton.addEventListener('click',() =>
+{
+    SetBreakfastStatus(false)
+    SetLunchStatus(false)
+    SetDinnerStatus(true)
+})
+function SetBreakfastStatus(Display)
+{
+    console.log("BreakFast Called: ", Display);
+    if (Display)
+    {
+        BreakfastList.forEach((Item) => 
+        {
+            Item.removeAttribute("hidden")
+        })
+    }
+    else
+    {
+        BreakfastList.forEach((Item) => 
+        {
+            Item.setAttribute("hidden", "True")
+        })
+    }
+}
+
+function SetLunchStatus(Display)
+{
+    console.log("Lunch Called: ", Display);
+    if (Display)
+    {
+        LunchList.forEach((Item) => 
+        {
+            Item.removeAttribute("hidden")
+        })
+    }
+    else
+    {
+        LunchList.forEach((Item) => 
+        {
+            Item.setAttribute("hidden", "True")
+        })
+    }
+}
+
+function SetDinnerStatus(Display)
+{
+    console.log("Dinner Called: ", Display);
+    if (Display)
+    {
+        DinnerList.forEach((Item) => 
+        {
+            Item.removeAttribute("hidden")
+        })
+    }
+    else
+    {
+        DinnerList.forEach((Item) => 
+        {
+            Item.setAttribute("hidden", "True")
+        })
+    }
+}
+
+ClearButton.addEventListener('click', () =>
+{
+    OrderList.innerHTML = ""
+})
+
+ConfirmButton.addEventListener('click', () =>
+{
+    let OrderDisplay = ""
+    SelectedItemList = document.querySelectorAll(".Order")
+    SelectedItemList.forEach((Item) => 
+    {
+        OrderDisplay += Item.querySelector(".OrderName").innerHTML + '\n'
+    });
+
+    alert(OrderDisplay)
+})
