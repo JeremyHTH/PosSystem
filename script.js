83575debb26abb761022d0bfaf62e485bca339b3
JeremyHTH <jeremy.hung56@gmail.com>
2023-11-26 15:57:27 +0800

    Update Menu

--- script.js
+++ script.js
@@ -2,6 +2,62 @@ const DisplayAllButton = document.getElementById("DisplayAll")
 const DisplayBreakfastButton = document.getElementById("DisplayBreakfast")
 const DisplayLunchButton = document.getElementById("DisplayLunch")
 const DisplayDinnerButton = document.getElementById("DisplayDinner")
+const DebugButton = document.getElementById("Debug")
+
+const MenuItemContainer = document.getElementById('ItemPage')
+
+var MenuItemList, DrinkOptionList, MainDishOptionList, SpecialOptionList;
+const LoadData = async () =>
+{
+    let P1 = await fetch('GeneratedJSON\\Item.json')
+        .then((response) => { return response.json()})
+        .then((json) => {MenuItemList = json})
+        
+    console.log(MenuItemList);
+
+    MenuItemList.forEach((FoodItem, Index) => {
+        MenuItemContainer.innerHTML += `<div class="Item" Breakfast> <h3 class="ItemName"> ${FoodItem.Name} </h3> </div>`
+    })
+
+    const AllList = document.querySelectorAll(".Item")
+
+    console.log(AllList.length);
+
+    AllList.forEach((Item, Index) =>{
+        Item.addEventListener('click', () =>
+        {
+            let Name = Item.querySelector(".ItemName").innerHTML;
+            console.log(Name);
+    
+            const CurrOrder = document.createElement('div');
+            CurrOrder.classList.add('Order');
+    
+            const OrderedItem = document.createElement('h1')
+            OrderedItem.classList.add("OrderName")
+            OrderedItem.innerHTML = Name
+            CurrOrder.appendChild(OrderedItem)
+    
+            const CancelButton = document.createElement('button')
+            CancelButton.classList.add("CancelButton")
+            CancelButton.innerHTML = 'Cancel'
+            CurrOrder.appendChild(CancelButton)
+    
+            OrderList.appendChild(CurrOrder)
+    
+            CancelButton.addEventListener('click', () =>
+            {
+                OrderList.removeChild(CurrOrder)
+            })
+    
+            OrderList.appendChild(CurrOrder)
+            
+            // let NewItem = `<div> <h1> ${Name} </h1> <button>Cancel</button> </div>`
+            // OrderList.innerHTML += NewItem
+            
+        })
+    })
+}
+LoadData()
 
 const AllList = document.querySelectorAll(".Item")
 const BreakfastList = document.querySelectorAll(".Item[Breakfast]")
@@ -13,9 +69,6 @@ const ConfirmButton = document.getElementById("ConfirmButton")
 const ClearButton = document.getElementById("ClearButton")
 
 console.log(AllList.length);
-console.log(BreakfastList.length);
-console.log(LunchList.length);
-console.log(DinnerList.length);
 
 AllList.forEach((Item, Index) =>{
     Item.addEventListener('click', () =>
@@ -148,3 +201,5 @@ ConfirmButton.addEventListener('click', () =>
 
     alert(OrderDisplay)
 })
+
+DebugButton.addEventListener('click', () => {console.log(MenuItemList);})
